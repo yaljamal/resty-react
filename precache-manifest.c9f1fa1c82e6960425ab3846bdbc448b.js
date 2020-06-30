a22776453abac8fa37cc148b6c576d49779b5594
mrsmarah <runner@fv-az18.qbyjgvl2j0re3nlt5rvbpnjiaf.cx.internal.cloudapp.net>
2020-06-30 01:10:26 +0000

    Updates

---
+++ precache-manifest.c9f1fa1c82e6960425ab3846bdbc448b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896ce831c14f83723a27171b93c5e6fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23f5851efa39d6e0f386",
+    "url": "/static/css/main.306b1162.chunk.css"
+  },
+  {
+    "revision": "d236e5ba6efd30a6912c",
+    "url": "/static/js/2.815e381e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.815e381e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f5851efa39d6e0f386",
+    "url": "/static/js/main.7b76cb42.chunk.js"
+  },
+  {
+    "revision": "b1f5858c5f98a8db1cb0",
+    "url": "/static/js/runtime-main.1f614100.js"
+  }
+]);
